0d4765632c9f5d0fb2e83e0121fc4c38f63be2be
Paola Kullaj <paola.kullaj@gmail.com>
2021-11-09 06:32:41 -0500

    removed spaces at the beginning and end of the input; checked if input empty

--- python/app.py
+++ python/app.py
@@ -4,19 +4,23 @@ seen_strings = {}
 def stringinate():
     while True:
         inputResult = input('Enter an input string (\'stats\' to see statistics, \'quit\' to exit): ')
-        if (inputResult.lower() == "quit"):
-            print('Exiting...\n')
-            break
-        elif (inputResult.lower() == "stats"):
-            print('\nStats:\n %s\n' % seen_strings)
-        else:
-            print('\nInput = %s' % inputResult)
-            print('Length = %s \n' % len(inputResult))
-
-            if inputResult in seen_strings:
-                seen_strings[inputResult] += 1
+        inputResult = inputResult.strip()
+        if inputResult:
+            if (inputResult.lower() == "quit"):
+                print('Exiting...\n')
+                break
+            elif (inputResult.lower() == "stats"):
+                print('\nStats:\n %s\n' % seen_strings)
             else:
-                seen_strings[inputResult] = 1
+                print('\nInput = %s' % inputResult)
+                print('Length = %s \n' % len(inputResult))
+
+                if inputResult in seen_strings:
+                    seen_strings[inputResult] += 1
+                else:
+                    seen_strings[inputResult] = 1
+        else:
+            print('No input given.')
 
 if __name__ == '__main__':
     stringinate()
